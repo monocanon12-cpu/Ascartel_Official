4ee94829006073250468b3824c185783e59ff9da
AsCartel Official <monocanon12-cpu>
2026-01-04 12:25:16 +0100

    Configure Firebase with real credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,12 @@
 // Configuration Firebase pour AsCartel
-// À REMPLACER avec vos vraies clés Firebase
-
 const firebaseConfig = {
-  apiKey: "VOTRE_API_KEY",
-  authDomain: "VOTRE_PROJECT_ID.firebaseapp.com",
-  projectId: "VOTRE_PROJECT_ID",
-  storageBucket: "VOTRE_PROJECT_ID.appspot.com",
-  messagingSenderId: "VOTRE_SENDER_ID",
-  appId: "VOTRE_APP_ID"
+  apiKey: "AIzaSyBZeZa13ZdgjfLdsxVDIU7rl_GNQXJ3f50",
+  authDomain: "ascartel-bdc28.firebaseapp.com",
+  projectId: "ascartel-bdc28",
+  storageBucket: "ascartel-bdc28.firebasestorage.app",
+  messagingSenderId: "262738114322",
+  appId: "1:262738114322:web:d6c008107102aaf8e7a5cb",
+  measurementId: "G-845H295SFJ"
 };
 
 // Export pour utilisation
